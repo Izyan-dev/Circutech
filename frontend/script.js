8b59796b9fbe691bb05b4f8f436b22af46fb8d8d
Izyan <izyanlirani5@gmail.com>
2025-04-02 19:58:43 +0530

    Initial commit12323? asd?

--- frontend/script.js
+++ frontend/script.js
@@ -35,3 +35,9 @@ function togglePassword() {
         passwordField.type = "password";
     }
 }
+
+function togglePassword() {
+    let passwordField = document.getElementById("password");
+    passwordField.type = passwordField.type === "password" ? "text" : "password";
+}
+
